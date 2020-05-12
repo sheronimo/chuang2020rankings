f66272cd026d9bc18fcc23a6670c52e18cdc65e3
Mouse <karmabutterfly309@gmail.com>
2020-05-12 15:15:12 +0700

    update trainee pics

--- js/main.js
+++ js/main.js
@@ -171,7 +171,7 @@ function displayProfile(d) {
 }
 
 function getImageSource(d) {
-    return "pics/" + d.name.replace(/ /g, "") + ".jpg";
+    return "pics/" + d.name.replace(/ /g, "") + ".png";
 }
 
 function getBackground(d) {
